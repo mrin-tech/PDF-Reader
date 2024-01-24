31fc3a300daf1b1d831cc4ee572a59d763adeab2
“Mrinmoyee <mrmishra@ucsc.edu>
2024-01-24 11:03:50 -0800

    Initial Commit

---
+++ PDF-Reader/PDF_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_ReaderApp.swift
+//  PDF-Reader
+//
+//  Created by Mrinmoyee Mishra on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PDF_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
